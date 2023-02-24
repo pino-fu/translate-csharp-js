69f1a09c58b85ec8362b0da50a977c5ac19105f9
Ben Hoberg <benjhoberg@gmail.com>
2023-02-24 13:43:30 -0600

    rolling-dice challenge complete. C# to JS

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,1 +1,18 @@
-// Put your code here
+const roller = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+for (i = 0; i < 10; i++) {
+    const roll1 = roller(1,7);
+    const roll2 = roller(1,7);
+
+    let message = `${roll1} + ${roll2} == ${roll1 + roll2}`
+
+    if (roll1 === roll2) {
+        message += `: DOUBLE!`
+    }
+
+    console.log(message)
+}
