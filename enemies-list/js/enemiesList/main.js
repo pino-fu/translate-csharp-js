2ab7b5133fc31cb5fde3259431cd57e9e083f422
Ben Hoberg <benjhoberg@gmail.com>
2023-02-24 11:06:32 -0600

    enemies challenge completed, C# to JS

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -1,1 +1,31 @@
-// Put your code here
+const enemies = [
+    {
+        firstName: "Bad",
+        lastName: "Guy",
+        offenses: ["poor turn signal usage", "leaving the seat up", "farting in elevators"],
+        isReallyHated: true
+    },
+    {
+        firstName: "Dr.",
+        lastName: "Evil",
+        offenses: ["not flushing", "spoiling movie plots", "always drunk"],
+        isReallyHated: false
+    },
+    {
+        firstName: "Jr.",
+        lastName: "Dev",
+        offenses: ["merging other peoples pull requests", "zombie code", "camera always off"],
+        isReallyHated: true
+    }
+];
+
+enemies.map(
+    (enemy) => {
+        if (enemy.isReallyHated) {
+            console.log(`${enemy.firstName} ${enemy.lastName} (REALLY HATE THIS GUY!)`);
+        } else {
+            console.log(`${enemy.firstName} ${enemy.lastName}`);
+            
+        }
+    }
+)
